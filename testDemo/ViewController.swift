0cfebd008310c83dd13be6a2fc96dd8ea4e4c37b
Vishnu <vishnup158@gmail.com>
2019-01-03 18:36:56 +0530

    Initial Commit

---
+++ testDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testDemo
+//
+//  Created by SpegaTechnology on 1/3/19.
+//  Copyright © 2019 SpegaTechnology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
